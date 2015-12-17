9fa49f781343b1aa9d15ad623f1bad63c2aadaee
itgsod-dennis-ozturk <dennis.ozturk@elev.it-gymnasiet.se>
2015-12-17 13:24:21 +0100

    first commit

---
+++ js/search.js
@@ -0,0 +1,17 @@
+function contains(text_one , text_two){
+    if (text_one.indexOf(text_two) != -1)
+        return true;
+}
+$("#searchText").keyup(function(){
+
+    var searchText = $("#searchText").val().toLowerCase();
+
+    $("ul li").each(function () {
+        if (!contains($(this).text().toLowerCase(), searchText))
+            $(this).hide();
+        else
+            $(this).show();
+
+    });
+});
+
